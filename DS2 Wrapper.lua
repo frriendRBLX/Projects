604cf39baaa01d362f98fa2351c2707ad3554d1d
frriend <73687008+frriendRBLX@users.noreply.github.com>
2021-08-07 14:23:01 -0600

    Update DS2 Wrapper.lua

--- DS2 Wrapper.lua
+++ DS2 Wrapper.lua
@@ -1,4 +1,5 @@
 local DS2Wrapper = {}
+DS2Wrapper.__index = DS2Wrapper
 
 --[[
     DataStore2 Wrapper by frriend
@@ -13,44 +14,48 @@ local DS2Wrapper = {}
 local DS2 = require(game.ReplicatedStorage.DataStore2) -- Path to Datastore 2 
 DS2.Combine("PlayerData", "Data")
 
-function DS2Wrapper:Register(DefaultDataFolder, PlayerDataFolder, Player)
-    task.spawn(function()
-        -- Create DataStore for Player --
-        local PlayerDataStore = DS2("Data", Player)
+function DS2Wrapper:Register(PlayerDataFolder, Player)
+	task.spawn(function()
+		local DefaultDataFolder = PlayerDataFolder:Clone()
 
-        -- Create a Default Dictionary to Compare To --
-        local DefaultData = {}
-        for _, item in ipairs(DefaultDataFolder:GetDescendants()) do
-            DefaultData[tostring(item.Name)] = item.Value
-        end
+		-- Create DataStore for Player --
+		local PlayerDataStore = DS2("Data", Player)
 
-        -- Get Player Data | If it doesn't exist, set it to the defaults --
-        local PlayerData = PlayerDataStore:Get(DefaultData)
+		-- Create a Default Dictionary to Compare To --
+		local DefaultData = {}
+		for _, item in ipairs(DefaultDataFolder:GetDescendants()) do
+			DefaultData[tostring(item.Name)] = item.Value
+		end
 
-        -- Verify Player Data --
-        for key, value in pairs(DefaultData) do
-            if PlayerData[key] == nil then
-                PlayerData[key] = value          
-            end
-        end
+		-- Get Player Data | If it doesn't exist, set it to the defaults --
+		local PlayerData = PlayerDataStore:Get(DefaultData)
 
-        -- Remove Depricated Data --
-        for key, _ in pairs(PlayerData) do
-            if DefaultData[key] == nil then
-                PlayerData[key] = nil
-            end
-        end
-        
-        -- Update/Listen to Player Cache --
-	for _, valueObject in ipairs(PlayerDataFolder:GetDescendants()) do
-            valueObject.Value = PlayerData[valueObject.Name]
+		-- Verify Player Data --
+		for key, value in pairs(DefaultData) do
+			if PlayerData[key] == nil then
+				PlayerData[key] = value          
+			end
+		end
 
-            valueObject.Changed:Connect(function(value)
-                PlayerData[valueObject.Name] = value
-                PlayerDataStore:Set(PlayerData)
-            end)
-        end
-    end)    
+		-- Remove Depricated Data --
+		for key, _ in pairs(PlayerData) do
+			if DefaultData[key] == nil then
+				PlayerData[key] = nil
+			end
+		end
+
+		-- Update/Listen to Player Cache --
+		for _, valueObject in ipairs(PlayerDataFolder:GetDescendants()) do
+			valueObject.Value = PlayerData[valueObject.Name]
+
+			valueObject.Changed:Connect(function(value)
+				PlayerData[valueObject.Name] = value
+				PlayerDataStore:Set(PlayerData)
+			end)
+		end
+		
+		DefaultDataFolder = nil
+	end)    
 end
 
 return DS2Wrapper
